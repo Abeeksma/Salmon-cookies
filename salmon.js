a5805dc562e8cfccb5e56427921cc902727219ff
Alexander <maniacallion@gmail.com>
2019-03-18 18:12:38 -0500

    mess of fucking bullshit i don't fully understand just commiting so i at least  feel like i did something before leaving class to go fail at home. Fuckadoodledoo

---
+++ salmon.js
@@ -0,0 +1,29 @@
+
+
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm',];
+
+var storePike = {
+  nameOfStore: '1st and Pike',
+  minCustomer: 23,
+  maxCustomer: 65,
+  averageCookiesPerSale: 6.3,
+  customersEachHour: [],
+  cookiesEachHour: [],
+  totalCookiesDaily: 0,
+
+  calccustomersEachHour: function() {
+    for(var i = 0; i < hours.length; i++) {
+      this.customersEachHour.push(Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer));
+    }
+  },
+  calccookiesEachHour: function() {
+    for(var i = 0; i < customersEachHour.length; i++) {
+      this.cookiesEachHour.push(Math.ceil(this.customersEachHour[i] * this.averageCookiesPerSale));
+    }
+  },
+  calcTotalCookies: function() {
+    for(var i = 0; i < cookiesEachHour.length; i++) {
+      this.totalCookiesDaily += cookiesEachHour[i];
+    }
+  },
+};
