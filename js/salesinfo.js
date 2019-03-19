9643e6e6cf24c4174d8f94ad33e0a953c35d9d22
Alexander <maniacallion@gmail.com>
2019-03-19 14:02:35 -0500

    table has been created need styling to make less nasty

--- js/salesinfo.js
+++ js/salesinfo.js
@@ -6,12 +6,12 @@ var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '
 //constructor function//
 
 function Store(nameOfStore, minCustomer, maxCustomer, averageCookiesPerSale) {
-  this.nameOfStore = nameOfStore,
-  this.minCustomer = minCustomer,
-  this.maxCustomer = maxCustomer,
-  this.averageCookiesPerSale = averageCookiesPerSale,
-  this.customersEachHour = [],
-  this.cookiesEachHour = [],
+  this.nameOfStore = nameOfStore;
+  this.minCustomer = minCustomer;
+  this.maxCustomer = maxCustomer;
+  this.averageCookiesPerSale = averageCookiesPerSale;
+  this.customersEachHour = [];
+  this.cookiesEachHour = [];
   this.totalCookiesDaily = 0;
 }
 
@@ -27,11 +27,54 @@ Store.prototype.calccookiesEachHour = function() {
   }
 };
 
-Store.prototype. calcTotalCookies = function() {
+Store.prototype.calcTotalCookies = function() {
   for(var i = 0; i < this.cookiesEachHour.length; i++) {
     this.totalCookiesDaily += this.cookiesEachHour[i];
   }
 };
+
+Store.prototype.render = function() {
+
+  this.calccustomersEachHour();
+  this.calccookiesEachHour();
+  this.calcTotalCookies();
+  var newListName = document.createElement('tr');
+  var newText = document.createTextNode(this.nameOfStore);
+  newListName.appendChild(newText);
+
+  for(var j = 0; j < hours.length; j++) {
+    var newListItem = document.createElement('td');
+    var newLineText = document.createTextNode(`Cookies: ${this.cookiesEachHour[j]}`);
+    newListItem.appendChild(newLineText);
+    newListName.appendChild(newListItem);
+  }
+
+  var totalCookies = document.createElement('td');
+  var totalCookiesText = document.createTextNode(`Total cookies: ${this.totalCookiesDaily}`);
+  totalCookies.appendChild(totalCookiesText);
+  newListName.appendChild(totalCookies);
+
+  var whereIStickIt = document.getElementsByTagName('table')[0];
+  whereIStickIt.appendChild(newListName);
+};
+
+function createTable() {
+  var table = document.createElement('table');
+  var tableRow = document.createElement('tr');
+  var locationHeadings = document.createElement('th');
+  locationHeadings.textContent = 'Location';
+  tableRow.appendChild(locationHeadings);
+
+  for(var i = 0; i <hours.length; i++) {
+    var tableHeading = document.createElement('th');
+    tableHeading.textContent =hours[i];
+    tableRow.appendChild(tableHeading);
+  }
+  table.appendChild(tableRow);
+  document.body.appendChild(table);
+}
+
+createTable();
 
 var storePike = new Store('1st and Pike', 23, 65, 6.3);
 var seatleCenter = new Store('Seattle Center', 11, 38, 3.7);
@@ -42,6 +85,11 @@ var alki = new Store('Alki', 2, 16, 4.6);
 
 var places = [storePike, seatleCenter, airport, capitolHill, alki];
 console.log(places);
+
+for(var i = 0; i < places.length; i++) {
+  var currentPlace = places[i];
+  currentPlace.render();
+}
 
 
 //to fix my decimal numbers if i need to... eg 9.2 is actually 9.199999999987459874252 fuck all)
