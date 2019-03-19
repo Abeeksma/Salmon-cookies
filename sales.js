aa30c046c1b7c2f4c4692e633f0e6e29cb23f11c
Alexander <maniacallion@gmail.com>
2019-03-18 21:20:52 -0500

    list items in uls and total cookies listed for each

--- sales.js
+++ sales.js
@@ -4,16 +4,25 @@ console.log('sale.js loaded');
 
 for(var i = 0; i < places.length; i++) {
   var currentPlace = places[i];
-  console.log(currentPlace.nameOfStore)
+  console.log(currentPlace.nameOfStore);
   var newListName = document.createElement('ul');
   var newText = document.createTextNode(currentPlace.nameOfStore);
   newListName.appendChild(newText);
 
+  for(var j = 0; j < hours.length; j++) {
+    console.log(currentPlace.cookiesEachHour[j]);
+    var newListItem = document.createElement('li');
+    var newLineText = document.createTextNode(`${hours[j]}: cookies ${currentPlace.cookiesEachHour[j]}`);
+    newListItem.appendChild(newLineText);
+    newListName.appendChild(newListItem);
+  }
+
+  var totalCookies = document.createElement('li');
+  var totalCookiesText = document.createTextNode(`Total cookies: ${currentPlace.totalCookiesDaily}`);
+  totalCookies.appendChild(totalCookiesText);
+  newListName.appendChild(totalCookies);
+
   var whereIStickIt = document.getElementsByTagName('body')[0];
   whereIStickIt.appendChild(newListName);
 
-  for(var j = 0; j < hours.length; j++) {
-    console.log(currentPlace.cookiesEachHour[j]);
-    
-  }
 }
