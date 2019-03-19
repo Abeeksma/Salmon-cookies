f0af6e7a704529e043cb91ee5b5b89fef36deb6e
Alexander <maniacallion@gmail.com>
2019-03-18 20:26:08 -0500

    header writes to sales.html

--- sales.js
+++ sales.js
@@ -1,4 +1,18 @@
 'use strict';
 
 console.log('sale.js loaded');
-console.log(places);
+
+for(var i = 0; i < places.length; i++) {
+  console.log(places[i].nameOfStore)
+  var newHeader = document.createElement('h2');
+  var newText = document.createTextNode(places[i].nameOfStore);
+  newHeader.appendChild(newText);
+
+  var whereIStickIt = document.getElementsByTagName('body')[0];
+
+  whereIStickIt.appendChild(newHeader);
+
+}
+
+
+
