aaef0277f694e2d629b7ae18f100929df98e328d
Alexander <maniacallion@gmail.com>
2019-03-20 21:26:02 -0500

    total cookie/hour row added (additions stack on top and it needs tweaking)

--- js/salesinfo.js
+++ js/salesinfo.js
@@ -3,7 +3,7 @@ console.log('saleinfo.js loaded');
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm',];
 
-//constructor function//
+//constructor function for orginal stores//
 
 function Store(nameOfStore, minCustomer, maxCustomer, averageCookiesPerSale) {
   this.nameOfStore = nameOfStore;
@@ -33,10 +33,12 @@ Store.prototype.calcTotalCookies = function() {
   }
 };
 
+///render to table shit///
 Store.prototype.render = function() {
   this.calccustomersEachHour();
   this.calccookiesEachHour();
   this.calcTotalCookies();
+  //this.totalCookiesEachHour();
   var newListName = document.createElement('tr');
   var nameInTd = document.createElement('td');
   var newText = document.createTextNode(this.nameOfStore);
@@ -60,6 +62,39 @@ Store.prototype.render = function() {
   whereIStickIt.appendChild(newListName);
 };
 
+////// total cookie row ///////////
+function TotalRow(){
+  this.rowData = [];
+}
+
+TotalRow.prototype.totalCookiesEachHour = function() {
+  var hourlyCookies = ['TOTAL'];
+  for(var i =0; i < hours.length; i++) {
+    var cookiesNao = 0;
+    for(var j = 0; j < places.length; j++ ) {
+      cookiesNao += places[j].cookiesEachHour[i];
+    } 
+    hourlyCookies.push(cookiesNao);
+  }
+  this.rowData = hourlyCookies;
+}; 
+
+TotalRow.prototype.render = function() {
+  this.totalCookiesEachHour();
+  var finalRow = document.createElement('tr');
+  
+  this.rowData.forEach(function(item){
+    var finalRowData = document.createElement('td');
+    var finalRowTextbits = document.createTextNode(item);
+    finalRowData.appendChild(finalRowTextbits);
+    finalRow.appendChild(finalRowData);
+  });
+
+  var whereIStickIt = document.getElementsByTagName('table')[0];
+  whereIStickIt.appendChild(finalRow)
+
+};
+
 function createTable() {
   var table = document.createElement('table');
   var tableRow = document.createElement('tr');
@@ -78,32 +113,32 @@ function createTable() {
 
 createTable();
 
+
 var storePike = new Store('1st and Pike', 23, 65, 6.3);
 var seatleCenter = new Store('Seattle Center', 11, 38, 3.7);
 var airport = new Store('SeaTac Airport', 3, 24, 1.2);
 var capitolHill = new Store('Capitol Hill', 20, 38, 2.3);
 var alki = new Store('Alki', 2, 16, 4.6);
 
+var totalRow= new TotalRow();
 
 var places = [storePike, seatleCenter, airport, capitolHill, alki];
 console.log(places);
+
 
 for(var i = 0; i < places.length; i++) {
   var currentPlace = places[i];
   currentPlace.render();
 }
 
+totalRow.render();
+
+
+
 
 //////////////////////// ADD NEW STORES FROM FORM MADNESS ////////////////////////////
 
 var fuckingform = document.getElementById('fuckingform');
-
-function AnotherStore (store, minimum, maximum, average) {
-  this.store = store;
-  this.minimum = minimum;
-  this.maximum = maximum;
-  this.average = average;
-}
 
 function addStoreFromForm (event) {
   event.preventDefault();
@@ -118,10 +153,3 @@ function addStoreFromForm (event) {
 }
 
 fuckingform.addEventListener('submit', addStoreFromForm);
-
-//to fix my decimal numbers if i need to... eg 9.2 is actually 9.199999999987459874252 fuck all)
-
-// function roundMyFloats(float) {
-//   var decimalNumber = Math.round(float * 10)/10;
-//   return decimalNumber;
-//}
